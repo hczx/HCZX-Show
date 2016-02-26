976a7c611bc33d6e9ebbf63d93a757ef09942f2d
Jam <991654396@qq.com>
2016-02-26 17:31:01 +0800

    Initial commit

---
+++ app/src/main/java/com/show/jam/hczx_show/MainActivity.java
@@ -0,0 +1,13 @@
+package com.show.jam.hczx_show;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
